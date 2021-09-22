b79acd279bc9644c8bb573d39953eb13ad35f7bf
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-09-22 15:33:46 -0400

    task drop down

--- public/javascripts/tasks-function.js
+++ public/javascripts/tasks-function.js
@@ -12,62 +12,62 @@ window.addEventListener("DOMContentLoaded", e => {
   const alltasksarrow = document.getElementById("ddownarrow-tasks");
   alltasksarrow.addEventListener("click", e => {
     alltaskslist.style.display === "block"
-<<<<<<< HEAD
-      ? (alltaskslist.style.display = "none")
-      : (alltaskslist.style.display = "block");
-
-    alltasksarrow.style.transform === "rotate(0deg)"
-      ? (alltasksarrow.style.transform = "rotate(-90deg)")
-      : (alltasksarrow.style.transform = "rotate(0deg)");
-=======
       ? (alltaskslist.style.display = "none")&&alltasksarrow.setAttribute("src","/images/blue-dright-arrow.PNG")
       : (alltaskslist.style.display = "block")&&alltasksarrow.setAttribute("src","/images/blue-ddown-arrow.PNG");
->>>>>>> main
   });
 
   const alllistslist = document.getElementById("options2list");
   const alllistsarrow = document.getElementById("ddownarrow-lists");
   alllistsarrow.addEventListener("click", e => {
     alllistslist.style.display === "block"
-<<<<<<< HEAD
-      ? (alllistslist.style.display = "none")
-      : (alllistslist.style.display = "block");
-
-      alllistsarrow.style.transform === "rotate(-90deg)"
-      ? (alllistsarrow.style.transform = "rotate(0deg)")
-      : (alllistsarrow.style.transform = "rotate(-90deg)");
-=======
       ? (alllistslist.style.display = "none")&&alllistsarrow.setAttribute("src","/images/blue-dright-arrow.PNG")
       : (alllistslist.style.display = "block")&&alllistsarrow.setAttribute("src","/images/blue-ddown-arrow.PNG");
->>>>>>> main
   });
 
   const alltagslist = document.getElementById("options3list");
   const alltagsarrow = document.getElementById("ddownarrow-tags");
   alltagsarrow.addEventListener("click", e => {
     alltagslist.style.display === "block"
-<<<<<<< HEAD
-      ? (alltagslist.style.display = "none")
-      : (alltagslist.style.display = "block");
-
-=======
       ? (alltagslist.style.display = "none")&&alltagsarrow.setAttribute("src","/images/blue-dright-arrow.PNG")
       : (alltagslist.style.display = "block")&&alltagsarrow.setAttribute("src","/images/blue-ddown-arrow.PNG");
->>>>>>> main
   });
 
   const alllanguagelist = document.getElementById("options4list");
   const alllanguagearrow = document.getElementById("ddownarrow-languages");
   alllanguagearrow.addEventListener("click", e => {
     alllanguagelist.style.display === "block"
-<<<<<<< HEAD
-      ? (alllanguagelist.style.display = "none")
-      : (alllanguagelist.style.display = "block");
-
-    alllanguagearrow.style.transform = "rotate(-90deg)";
-=======
       ? (alllanguagelist.style.display = "none")&&alllanguagearrow.setAttribute("src","/images/blue-dright-arrow.PNG")
       : (alllanguagelist.style.display = "block")&&alllanguagearrow.setAttribute("src","/images/blue-ddown-arrow.PNG");
->>>>>>> main
   });
+
+  // Functionality adding button or create task but not active until typing begins
+  const input = document.getElementById("tasks-input");
+  const addTaskButton = document.getElementById("tasks-button");
+  input.addEventListener("click", e => {
+    input.removeAttribute("placeholder");
+    addTaskButton.classList.remove("task-hidden");
+
+  });
+
+  input.addEventListener("change", (e) => {
+    if (e.target.value){
+      addTaskButton.disabled = false
+    }
+  });
+
+  const feature = document.querySelectorAll(".attribute");
+  const test = document.getElementById("test-div");
+  input.addEventListener("keypress", e => {
+    feature.forEach(el => {
+      el.classList.remove("task-hidden");
+    })
+    test.classList.remove("task-hidden");
+  })
+
+
+
+
+  
 });
+
+
