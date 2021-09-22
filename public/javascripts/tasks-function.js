f1b7021dfab2832fcf5a30e407aaa621d5cdbf01
William <willziller@gmail.com>
2021-09-21 19:50:42 -0500

    Possible final logo confirmed

--- public/javascripts/tasks-function.js
+++ public/javascripts/tasks-function.js
@@ -2,24 +2,24 @@ window.addEventListener("DOMContentLoaded", (e) => {
   const alltaskslist = document.getElementById("options1list");
   const alltasksarrow = document.getElementById("ddownarrow-tasks");
   alltasksarrow.addEventListener("click", (e) => {
-    alltaskslist.style.display === ""
+    alltaskslist.style.display === "block"
       ? (alltaskslist.style.display = "none")
-      : (alltaskslist.style.display = "");
+      : (alltaskslist.style.display = "block");
   });
 
   const alllistslist = document.getElementById("options2list");
   const alllistsarrow = document.getElementById("ddownarrow-lists");
   alllistsarrow.addEventListener("click", (e) => {
-    alllistslist.style.display === ""
+    alllistslist.style.display === "block"
       ? (alllistslist.style.display = "none")
-      : (alllistslist.style.display = "");
+      : (alllistslist.style.display = "block");
   });
 
   const alltagslist = document.getElementById("options3list");
   const alltagsarrow = document.getElementById("ddownarrow-tags");
   alltagsarrow.addEventListener("click", (e) => {
-    alltagslist.style.display === ""
+    alltagslist.style.display === "block"
       ? (alltagslist.style.display = "none")
-      : (alltagslist.style.display = "");
+      : (alltagslist.style.display = "block");
   });
 });
